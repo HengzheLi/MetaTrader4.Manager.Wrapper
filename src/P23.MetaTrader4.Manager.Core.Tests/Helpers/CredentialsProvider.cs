ff3df547d3f9d584999120f6771d39df62f20e29
HectorLi <hectorli@seismic.com>
2020-10-21 12:09:53 +0800

    add config for core build

--- src/P23.MetaTrader4.Manager.Core.Tests/Helpers/CredentialsProvider.cs
+++ src/P23.MetaTrader4.Manager.Core.Tests/Helpers/CredentialsProvider.cs
@@ -3,18 +3,28 @@ using System.IO;
 using P23.MetaTrader4.Manager.Contracts;
 using System.Configuration;
 using System.Reflection;
+using Microsoft.Extensions.Configuration;
 
 namespace P23.MetaTrader4.Manager.Tests.Helpers
 {
     internal static class TestHelpers
     {
+        static IConfiguration config;
+
+        static TestHelpers()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            config = builder.Build();
+        }
+
         private static readonly Lazy<ConnectionParameters> LazyParameters =
             new Lazy<ConnectionParameters>(() =>
             {
                 return new ConnectionParameters {
-                    Login = int.Parse(ConfigurationManager.AppSettings["account"]),
-                    Password = ConfigurationManager.AppSettings["password"],
-                    Server = ConfigurationManager.AppSettings["ipaddress"]
+                    Login = int.Parse(config["account"]),
+                    Password = config["password"],
+                    Server = config["ipaddress"]
                 };
             });
 
